11b1086916b90a49c67ba0c60a26118921b71a35
PatrykKruppa <128226968+PatrykKruppa@users.noreply.github.com>
2023-06-30 23:36:46 +0200

    Add files via upload

---
+++ 7.1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//Napisz funkcję, której zadaniem będzie formatowanie czasu na ekranie
+//w formacie HH::MM:SS (godziny, minuty, sekundy, 2 znaki na każdą pozycję).
+// Funkcja powinna przyjmować jako argument liczbę całkowitą wyrażacącą
+// czas jaki upłynął od godzinny 00:00:00 w samych sekundach np.: dla 3
+//32 wypisze 00:05:32.
+
+class Program
+{
+    static void Main()
+    {
+        int czas = 332;
+        string sformatowanyCzas = FormatujCzas(czas);
+        Console.WriteLine(sformatowanyCzas);
+    }
+
+    static string FormatujCzas(int czas)
+    {
+        int godziny = czas / 3600;
+        int minuty = (czas % 3600) / 60;
+        int sekundy = czas % 60;
+
+        string sGodziny = godziny.ToString("D2");
+        string sMinuty = minuty.ToString("D2");
+        string sSekundy = sekundy.ToString("D2");
+
+        string sformatowanyCzas = $"{sGodziny}:{sMinuty}:{sSekundy}";
+
+        return sformatowanyCzas;
+    }
+}
---
+++ 7.2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            double x1 = 3.5;
+            double y1 = 4.5;
+            double x2 = 5.5;
+            double y2 = 8.5;
+            double dlugosc = ObliczDlugoscOdcinka(x1, y1, x2, y2);
+            Console.WriteLine("Długość odcinka: " + dlugosc);
+        }
+
+        static double ObliczDlugoscOdcinka(double x1, double y1, double x2, double y2)
+        {
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+            double dlugosc = Math.Sqrt(dx * dx + dy * dy);
+            return dlugosc;
+        }
+    }
+}
